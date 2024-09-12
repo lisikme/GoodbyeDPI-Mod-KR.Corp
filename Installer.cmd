4c832aef1a71d267cf17202d35c2b8df2601bdab
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-13 02:50:06 +0300

    update

--- Installer.cmd
+++ Installer.cmd
@@ -4,7 +4,7 @@ start /b powershell -command "&{$w=(get-host).ui.rawui;$w.buffersize=@{width=177
 set _arch=x86
 IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
 IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
-echo [90mровека установки службы GoodbyeDPI [0m------------------------------[91m
+echo [90mПровека установки службы GoodbyeDPI [0m-----------------------------[91m
 sc qdescription "GoodbyeDPI"
 echo [0m-----------------------------------------------------------------
 echo  [93mЭтот скрипт следует запускать с правами администратора.
@@ -14,16 +14,17 @@ setlocal
 cls
 :begin
 echo [90mУтилита настройки службы GoodbyeDPI [0m-----------------------------
-echo   [93m1. [92mУстановить службу GoodbyeDPI без DNS
-echo   [93m2. [92mУстановить службу GoodbyeDPI с ЯндексDNS
+echo   [93m1 - [92mУстановить службу GoodbyeDPI без DNS
+echo   [93m2 - [92mУстановить службу GoodbyeDPI с ЯндексDNS
 echo;
-echo   [93m7. [94mОбновить список доменов от KetaruCorp
-echo   [93m8. [96mОбновить список доменов от ValdikSS
-echo   [93m9. [91mУдалить службу GoodbyeDPI
-echo   [33m0. [31mЗакрыть панель настроек[0m
+echo   [93m6 - [33mОбновить установк службы GoodbyeDPI
+echo   [93m7 - [94mОбновить список доменов от KetaruCorp
+echo   [93m8 - [96mОбновить список доменов от ValdikSS
+echo   [93m9 - [91mУдалить службу GoodbyeDPI
+echo   [93m0 - [31mЗакрыть панель настроек[0m
 echo [0m-----------------------------------------------------------------
 echo [90m Нажмите на кнопку из списка для продолжения...
-choice /n /c 127890 > nul
+choice /n /c 1267890 > nul
 set rmFunc=%errorlevel%
 for %%I in (1 2 3 4 5 6 7 8 9 0) do if #%rmFunc%==#%%I goto run%%I
 echo %rmFunc%
@@ -34,7 +35,7 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
 sc delete "GoodbyeDPI" > nul
-sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\russia-youtube.txt\"" start= "auto"
 sc description "GoodbyeDPI" "Блокировщик пассивной глубокой проверки пакетов и утилита обхода активного DPI"
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
@@ -52,7 +53,7 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
 sc delete "GoodbyeDPI" > nul
-sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\russia-youtube.txt\"" start= "auto"
 sc description "GoodbyeDPI" "Блокировщик пассивной глубокой проверки пакетов и утилита обхода активного DPI c ЯндексDNS"
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
@@ -65,6 +66,19 @@ echo;
 goto begin
 
 :run3
+rem Обновить программу
+cls
+echo [90mЛог выполения [0m---------------------------------------------------[91m
+echo Оновление установщика службы GoodbyeDPI
+bitsadmin /transfer blacklist https://raw.githubusercontent.com/lisikme/GoodbyeDPI-Mod-KR.Corp/main/Installer.cmd "%CD%\Installer.cmd" > nul
+echo;
+echo [90mСтатус выполения скрипта [0m----------------------------------------
+echo  [93mУстановщик службы GoodbyeDPI успешно обновлен!
+echo [0m-----------------------------------------------------------------
+echo;
+goto begin
+
+:run4
 rem Обновить список доменов для GoodbyeDPI
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
@@ -82,7 +96,7 @@ echo [0m-----------------------------------------------------------------
 echo;
 goto begin
 
-:run4
+:run5
 rem Обновить список доменов для GoodbyeDPI
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
@@ -100,7 +114,7 @@ echo [0m-----------------------------------------------------------------
 echo;
 goto begin
 
-:run5
+:run6
 rem Удаление службы
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
