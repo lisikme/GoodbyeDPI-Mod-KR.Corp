917f676dc3f9527d6ecbcc79f744878aec81c2a3
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-13 06:28:14 +0300

    66767687687687

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -1,9 +1,9 @@
-@echo off
-ren C:\GoodbyeDPI-Mod-KR.Corp-main goodbyedpi
+@REM @echo off
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip', 'C:\goodbyedpi.zip')" > nul
 powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
 powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
+Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
 powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
 powershell Remove-Item -LiteralPath "C:\goodbyedpi\srv" -Force -Recurse > nul
-@REM @RD /S /Q "C:\GoodbyeDPI-Mod-KR.Corp-main"
+rmdir /s /q "C:\GoodbyeDPI-Mod-KR.Corp-main"
 pause
