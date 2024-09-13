9233e499dce6f040d81cc6272e05b3787f2d6254
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-13 07:14:20 +0300

    77989890-0-

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -1,5 +1,4 @@
-@REM @echo off
-
+@echo off
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe', 'C:\Windows\Temp\unlocker.exe')" > nul
 powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
 start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
@@ -8,5 +7,4 @@ powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod
 powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
 Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
 powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
-start C:\Windows\Temp\unlocker.exe "C:\GoodbyeDPI-Mod-KR.Corp-main"
-pause 
+start C:\Windows\Temp\unlocker.exe "C:\GoodbyeDPI-Mod-KR.Corp-main" "C:\goodbyedpi.zip"
