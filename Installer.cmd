d073d94ff6dc99856e846f4e034daba78520a060
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-13 19:45:10 +0300

    888

--- Installer.cmd
+++ Installer.cmd
@@ -39,13 +39,12 @@ echo   [93m9 - [91mУдалить службу GoodbyeDPI
 echo   [93m0 - [31mЗакрыть панель настроек службы GoodbyeDPI
 echo [0m-----------------------------------------------------------------
 echo [90m Нажмите на кнопку из списка для продолжения...
-choice /n /c 1267890 > nul
+choice /n /c 123467890 > nul
 set rmFunc=%errorlevel%
 for %%I in (1 2 3 4 5 6 7 8 9 0) do if #%rmFunc%==#%%I goto run%%I
 echo %rmFunc%
 pause
 :run1
-rem ??????????????????? ????????????
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
@@ -78,8 +77,41 @@ echo  [93mСлужба GoodbyeDPI успешно установлена c ЯндексDNS!
 echo [0m-----------------------------------------------------------------
 echo;
 goto begin
-
 :run3
+cls
+echo [90mЛог выполения [0m---------------------------------------------------[91m
+sc stop "GoodbyeDPI" > nul
+sc delete "GoodbyeDPI" > nul
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -5 -e1 -q --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+sc description "GoodbyeDPI" "Блокировщик пассивной глубокой проверки пакетов и утилита обхода активного DPI"
+start /min sc start "GoodbyeDPI"
+sc query "GoodbyeDPI"
+POPD
+echo;
+echo [90mСтатус выполения скрипта [0m----------------------------------------
+echo  [93mСлужба GoodbyeDPI успешно установлена!
+echo [0m-----------------------------------------------------------------
+echo;
+goto begin
+
+:run4
+cls
+echo [90mЛог выполения [0m---------------------------------------------------[91m
+sc stop "GoodbyeDPI" > nul
+sc delete "GoodbyeDPI" > nul
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -5 -e1 -q --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+sc description "GoodbyeDPI" "Блокировщик пассивной глубокой проверки пакетов и утилита обхода активного DPI c ЯндексDNS"
+start /min sc start "GoodbyeDPI"
+sc query "GoodbyeDPI"
+POPD
+echo;
+echo [90mСтатус выполения скрипта [0m----------------------------------------
+echo  [93mСлужба GoodbyeDPI успешно установлена c ЯндексDNS!
+echo [0m-----------------------------------------------------------------
+echo;
+goto begin
+
+:run5
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 echo  Обновление службы GoodbyeDPI...
@@ -93,7 +125,7 @@ echo;
 exit
 goto begin
 
-:run4
+:run6
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 echo  Оновление бызы обхода блокировок от KetaruCorp...
@@ -110,7 +142,7 @@ echo [0m-----------------------------------------------------------------
 echo;
 goto begin
 
-:run5
+:run7
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 echo  Оновление бызы обхода блокировок ValdikSS...
@@ -127,7 +159,7 @@ echo [0m-----------------------------------------------------------------
 echo;
 goto begin
 
-:run6
+:run8
 cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
