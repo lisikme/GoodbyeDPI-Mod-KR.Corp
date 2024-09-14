9d087f3807b36487839538c2340cd378cef51192
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-14 15:02:18 +0300

    999

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -1,11 +1,13 @@
 @REM @echo off
 @REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe', 'C:\Windows\Temp\unlocker.exe')" > nul
-powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
+@REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
 @REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip', 'C:\goodbyedpi.zip')" > nul
-powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
-start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
-powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
-Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
-powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
-start C:\Windows\Temp\unlocker.exe "C:\GoodbyeDPI-Mod-KR.Corp-main" "C:\goodbyedpi.zip"
+@REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
+certutil -urlcache -split -f "https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip" > nul
+certutil -urlcache -split -f "https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip" > nul
+@REM start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
+@REM powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
+@REM Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
+@REM powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
+@REM start C:\Windows\Temp\unlocker.exe "C:\GoodbyeDPI-Mod-KR.Corp-main" "C:\goodbyedpi.zip"
 pause 
