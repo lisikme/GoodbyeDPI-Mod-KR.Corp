6792842e31976e010ec0ca26ddf7cf1ba3b71b39
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-14 23:32:17 +0300

    6666

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -1,6 +1,12 @@
 @echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KRCorp/archive/refs/heads/main.zip', 'C:\Windows\Temp\goodbyedpi.zip')" > nul
-powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KRCorp/archive/refs/heads/main.zip -OutFile C:\Windows\Temp\good.zpi" > nul
-powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
-Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
+
+powershell Remove-Item C:\goodbyedpi -Recurse -Force
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KRCorp/archive/refs/heads/main.zip', 'C:\Windows\Temp\goodtemp.zip')"
+powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KRCorp/archive/refs/heads/main.zip -OutFile C:\Windows\Temp\goodtemp.zip"
+
+powershell Expand-Archive C:\Windows\Temp\goodtemp.zip -DestinationPath C:\Windows\Temp\ -Force
+powershell Move-Item -Path C:\Windows\Temp\GoodbyeDPI-Mod-KRCorp-main -Destination C:\goodbyedpi -Force
+
 powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
+
+pause
