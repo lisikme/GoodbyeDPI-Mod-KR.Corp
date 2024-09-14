f493540f30e41bfe4ead36cd3d01452e37412b36
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-14 14:51:50 +0300

    7777

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -1,10 +1,11 @@
-@echo off
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe', 'C:\Windows\Temp\unlocker.exe')" > nul
-@REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
+@REM @echo off
+@REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe', 'C:\Windows\Temp\unlocker.exe')" > nul
+powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
 start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip', 'C:\goodbyedpi.zip')" > nul
-@REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
+@REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip', 'C:\goodbyedpi.zip')" > nul
+powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
 powershell Expand-Archive C:\goodbyedpi.zip -DestinationPath C:\
 Rename "C:\GoodbyeDPI-Mod-KR.Corp-main" "goodbyedpi"
 powershell Start-Process -FilePath "C:\goodbyedpi\Installer.cmd" > nul
 start C:\Windows\Temp\unlocker.exe "C:\GoodbyeDPI-Mod-KR.Corp-main" "C:\goodbyedpi.zip"
+pause
