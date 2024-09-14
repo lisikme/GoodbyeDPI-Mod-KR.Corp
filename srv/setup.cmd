f9d6b1349cc9252196b832e66135c4a5e594d079
DESKTOP-NF7DAMK\lisikme <lisikme>
2024-09-14 15:19:28 +0300

    6666

--- srv/setup.cmd
+++ srv/setup.cmd
@@ -3,7 +3,7 @@
 @REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe -OutFile C:\Windows\Temp\unlocker.exe" > nul
 @REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip', 'C:\goodbyedpi.zip')" > nul
 @REM powershell -Command "Invoke-WebRequest https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip -OutFile C:\goodbyedpi.zip" > nul
-@REM start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
+start C:\Windows\Temp\unlocker.exe "C:\goodbyedpi"
 certutil -urlcache -split -f "https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/archive/refs/heads/main.zip" C:\Windows\Temp\goodbyedpi.zip > nul
 certutil -urlcache -split -f "https://github.com/lisikme/GoodbyeDPI-Mod-KR.Corp/raw/main/srv/Unlocker.exe" C:\Windows\Temp\unlocker.exe > nul
 powershell Expand-Archive C:\Windows\Temp\goodbyedpi.zip -DestinationPath C:\
