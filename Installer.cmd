6b640cc9caba771381a6498c3be87c99d9aa94fa
lisik.me (Лисик Ми) <dev@ketaru.com>
2024-10-08 19:18:48 +0300

    аппол

--- Installer.cmd
+++ Installer.cmd
@@ -1,12 +1,14 @@
 @echo off
-start /min sc start "GoodbyeDPI" > nul
 cd C:\goodbyedpi > nul
+start /min sc start "GoodbyeDPI" > nul
 PUSHD "%~dp0"
 
 @REM set hexgen = 5
 @REM set hex = 160301FFFF01FFFFFF0303594F5552204144564552544953454D454E542048455245202D202431302F6D6F000000000009000000050003000000
-set hexgen = 29
-set hex = 4e4e2156ccb0334129a8788a4ecc34c92c0ef786952596e39d42d3f4d57eb850f0dfd52da3ad7d9690b13e443013b971aa765321d3f746a5a8963f2e033878a3a7a1ffeb41fff60da6a13bbcf10100d52e2b0b5ca73081876be05c57e9a75ef8ef72d1467f85f8be1bb70bf376993367bdfa2c147a0327ca4a4400ab98ffd40809876e98df37c1604e7649743ab37675b663f0c4fc06c4b7c6553d2f1dab53b7f55ed753ed2576daceb3d498db83519140d98289d4fc1b9a181315243069a21eae1848390539c844dcdc0a6040f56e054f8fdeb45359c5d9efbb7c39545f20ce37145b1ed9ecd101d46e143d16c69e2226576f99f0f4fb86bc6316f911c576459b3539e14abe617126c7239c43439baa5375c0685b465773a3b094754edb65a2df9a
+@REM set hexgen = 29
+@REM set hex = 4e4e2156ccb0334129a8788a4ecc34c92c0ef786952596e39d42d3f4d57eb850f0dfd52da3ad7d9690b13e443013b971aa765321d3f746a5a8963f2e033878a3a7a1ffeb41fff60da6a13bbcf10100d52e2b0b5ca73081876be05c57e9a75ef8ef72d1467f85f8be1bb70bf376993367bdfa2c147a0327ca4a4400ab98ffd40809876e98df37c1604e7649743ab37675b663f0c4fc06c4b7c6553d2f1dab53b7f55ed753ed2576daceb3d498db83519140d98289d4fc1b9a181315243069a21eae1848390539c844dcdc0a6040f56e054f8fdeb45359c5d9efbb7c39545f20ce37145b1ed9ecd101d46e143d16c69e2226576f99f0f4fb86bc6316f911c576459b3539e14abe617126c7239c43439baa5375c0685b465773a3b094754edb65a2df9a
+set hexgen = 5
+set hex = 160301FFFF01FFFFFF0303594F5552204144564552544953454D454E542048455245202D202431302F6D6F000000000009000000050003000000
 
 echo [93mПроверка прав администратора...
 echo [0m-----------------------------------------------------------------
@@ -19,8 +21,9 @@ if %errorLevel% == 0 (
     echo [0m-----------------------------------------------------------------
     echo;
     echo [90mВы должны открыть этот скрипт от имени администратора
+    echo [90mили вы можете открыть без прав администратора
+    echo [90mно некторые функции могут работать негоректно!
     pause > nul
-    exit
 )
 
 echo;
@@ -54,13 +57,16 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
 sc delete "GoodbyeDPI" > nul
+sc stop "WinDivert" > nul
+sc delete "WinDivert" > nul
 @REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
 @REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -p -r -s -f2 -e2 -m --reverse-frag --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
-sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -f 2 -e 2 --native-frag --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex%" start= "auto"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -9 -e1 -q --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex%" start= "auto"
+@REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -f 2 -e 2 --native-frag --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex%" start= "auto"
 sc description "GoodbyeDPI" "Служба утилиты GoodbyeDPI для обхода блокировок интернет сайтов. - Univesal"
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
-POPD
+popd
 echo;
 echo [90mСтатус выполения скрипта [0m----------------------------------------
 echo  [93mСлужба GoodbyeDPI (Univesal) успешно установлена!
@@ -73,13 +79,16 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
 sc delete "GoodbyeDPI" > nul
+sc stop "WinDivert" > nul
+sc delete "WinDivert" > nul
 @REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -7 -e1 --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
 @REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -p -r -s -f2 -e2 -m --reverse-frag --dns-addr 77.88.8.8 --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
-sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -f 2 -e 2 --native-frag --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex% --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -9 -e1 -q --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex% --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
+@REM sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -f 2 -e 2 --native-frag --max-payload 1200 --fake-gen %hexgen% --fake-from-hex %hex% --blacklist \"%CD%\russia-blacklist.txt\" --blacklist \"%CD%\ketaru-blacklist.txt\"" start= "auto"
 sc description "GoodbyeDPI" "Служба утилиты GoodbyeDPI для обхода блокировок интернет сайтов. - Modify by KetaruCorp"
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
-POPD
+popd
 echo;
 echo [90mСтатус выполения скрипта [0m----------------------------------------
 echo  [93mСлужба GoodbyeDPI (Modify by KetaruCorp) успешно установлена!
@@ -117,11 +126,11 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 echo  Обновление базы обхода блокировок от KetaruCorp...
 bitsadmin /transfer blacklist https://raw.githubusercontent.com/lisikme/GoodbyeDPI-Mod-KRCorp/main/retaru-blacklist.txt "%CD%\ketaru-blacklist.txt" > nul
-POPD
+popd
 sc stop "GoodbyeDPI" > nul
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
-POPD
+popd
 echo;
 echo [90mСтатус выполения скрипта [0m----------------------------------------
 echo  [93mБазы обхода блокировок от KetaruCorp успешно обновлены!
@@ -134,11 +143,11 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 echo  Обновление базы обхода блокировок ValdikSS...
 bitsadmin /transfer blacklist https://p.thenewone.lol/domains-export.txt "%CD%\russia-blacklist.txt" > nul
-POPD
+popd
 sc stop "GoodbyeDPI" > nul
 start /min sc start "GoodbyeDPI"
 sc query "GoodbyeDPI"
-POPD
+popd
 echo;
 echo [90mСтатус выполения скрипта [0m----------------------------------------
 echo  [93mБазы обхода блокировок от ValdikSS успешно обновлены!
@@ -151,8 +160,10 @@ cls
 echo [90mЛог выполения [0m---------------------------------------------------[91m
 sc stop "GoodbyeDPI" > nul
 sc delete "GoodbyeDPI" > nul
+sc stop "WinDivert" > nul
+sc delete "WinDivert" > nul
 sc query "GoodbyeDPI"
-POPD
+popd
 echo [90mСтатус выполения скрипта [0m----------------------------------------
 echo  [93mСлужба GoodbyeDPI успешно удалена!
 echo [0m-----------------------------------------------------------------
